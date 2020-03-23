458bc54872b92fe9f02dce78183a5be291bea55c
Nexocentric Studios Administrator <admin@nexocentric.com>
2020-03-23 20:03:54 +0000

    Deployed website. Made changes to the initial template file

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,6 +11,16 @@ function closeModal()
 
 function showModal(name)
 {
+
+	var radios = document.getElementsByName('pill');
+
+	for (var index = 0, length = radios.length; index < length; index++) {
+		if (radios[index].checked) {
+			if (radios[index].value == "blue") { name = "fail"; }
+			break;
+		}
+	}
+
 	let contactForm = document.getElementById(name + "-modal");
 	contactForm.classList.add("is-active");
 }
